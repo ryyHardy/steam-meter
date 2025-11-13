21297316ad571b04d4290cd107c5098abb5a0384
ryyHardy <ryyHardy@gmail.com>
2025-11-13 16:51:14 -0500

    removed src_py

--- src_py/config.py
+++
@@ -1,6 +0,0 @@
-import os
-from pathlib import Path
-
-ROOT = Path(__file__).resolve().parents[1]
-DATA_DIR = Path(os.environ.get("STEAMMETER_DATA", ROOT / "data"))
-# ^ Allows user to customize data path with an environment variable
